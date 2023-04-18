ca7d0aab15ae15c348be14fda8472e0a2981b9c3
Tarek Saad <saadfouad1976@gmail.com>
2023-04-18 10:52:01 +0200

    dark mood

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ dark_Icon.addEventListener("click", () => {
     dark_Icon.src = "img/moon.png";
   }
 });
+
+// DarkMode With Local Storage
+let check = document.getElementById("Dark_check");
+
+if (localStorage.getItem("darkMode") === null) {
+  localStorage.setItem("darkMode", "false");
+}
+
+// when check
+
+function WhenChange() {
+  if (localStorage.getItem("darkMode") === "false") {
+    document.body.classList.toggle("dark_theme");
+  } else {
+    document.body.classList.remove("dark_theme");
+  }
+}
+
+// when change check
+function Change_Check() {
+  if (localStorage.getItem("darkMode") === "false") {
+    localStorage.setItem("darkMode", "true");
+    WhenChange();
+  } else {
+    localStorage.setItem("darkMode", "false");
+    WhenChange();
+  }
+}
